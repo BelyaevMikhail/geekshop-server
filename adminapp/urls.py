8dea3524df57a0f4af212461fa4fc0c5ffe4f035
Beleaev Mikhail <ivzh.ehb@Yandex.ru>
2021-05-24 12:47:47 +0300

    added remove users

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from adminapp.views import index, admin_users_read, admin_users_create, admin_users_update
+from adminapp.views import index, admin_users_read, admin_users_create, admin_users_update, admin_users_remove
 
 app_name = 'adminapp'
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin-users-read/', admin_users_read, name='admin_users_read'),
     path('admin-users-create/', admin_users_create, name='admin_users_create'),
     path('admin-users-update/<int:pk>/', admin_users_update, name='admin_users_update'),
-    path('admin-users-remove/<int:pk>/', UserDeleteView.as_view(), name='admin_users_remove'),
+    path('admin-users-remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
 ]
